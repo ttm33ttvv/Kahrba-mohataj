e167b96fbc6cd7013e44764c90ad363a857b8d93
ttm33ttvv <ttmttvpnv@gmail.com>
2026-09-03 04:11:25 +0300

    Set namespace to com.aistudio.powerstation.mgrz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 android {
-  namespace = "com.example"
+  namespace = "com.aistudio.powerstation.mgrz"
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
